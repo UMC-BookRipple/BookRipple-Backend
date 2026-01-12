f198c000a41a95d22ca0e324957ffa00f9c90895
sseobi16 <jseobi3951@naver.com>
2026-01-12 23:44:40 +0900

    feat:  질문 관련 에러 코드 예시 #1

---
+++ src/main/java/com/bookripple/api/common/code/QuestionErrorCode.java
@@ -0,0 +1,34 @@
+package com.bookripple.api.common.code;
+
+import com.bookripple.api.common.code.BaseErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum QuestionErrorCode implements BaseErrorCode {
+    QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "QUESTION_404", "질문을 찾을 수 없습니다."),
+    QUESTION_FORBIDDEN(HttpStatus.FORBIDDEN, "QUESTION_403", "질문에 대한 권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    QuestionErrorCode(HttpStatus httpStatus, String code, String message) {
+        this.httpStatus = httpStatus;
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
