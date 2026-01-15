3fac7bc921909b9230eef3040489cb3160adab2d
오영훈 <skek2412@naver.com>
2026-01-15 23:07:34 +0900

    feat: book 엔티티 추가 #3

---
+++ src/main/java/com/bookripple/api/domain/book/entity/Book.java
@@ -0,0 +1,27 @@
+package com.bookripple.api.domain.book.entity;
+
+import com.bookripple.api.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Builder
+public class Book extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+}
