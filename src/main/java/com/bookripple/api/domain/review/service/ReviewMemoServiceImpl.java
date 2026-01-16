028fb39273d6ae1244d424731dd93346395c5ff9
오영훈 <skek2412@naver.com>
2026-01-16 19:34:41 +0900

    feat: 에러 코드들 common.code로 이동 #7

--- src/main/java/com/bookripple/api/domain/review/service/ReviewMemoServiceImpl.java
+++ src/main/java/com/bookripple/api/domain/review/service/ReviewMemoServiceImpl.java
@@ -1,12 +1,12 @@
 package com.bookripple.api.domain.review.service;
 
+import com.bookripple.api.common.code.ReviewErrorCode;
+import com.bookripple.api.common.error.ApiException;
 import com.bookripple.api.domain.member.entity.Member;
 import com.bookripple.api.domain.member.repository.MemberRepository;
 import com.bookripple.api.domain.review.converter.ReviewMemoConverter;
 import com.bookripple.api.domain.review.entity.Review;
 import com.bookripple.api.domain.review.entity.ReviewMemo;
-import com.bookripple.api.domain.review.exception.ReviewException;
-import com.bookripple.api.domain.review.exception.code.ReviewErrorCode;
 import com.bookripple.api.domain.review.repository.ReviewMemoRepository;
 import com.bookripple.api.domain.review.repository.ReviewRepository;
 import com.bookripple.api.global.converter.GlobalConverter;
@@ -26,11 +26,11 @@ public class ReviewMemoServiceImpl implements ReviewMemoService {
   @Override
   public IdRes createReviewMemo(Long reviewId, Long memberId, ContentReq request) {
     Review review = reviewRepository.findById(reviewId)
-        .orElseThrow(() -> new ReviewException(ReviewErrorCode.NO_REVIEW));
+        .orElseThrow(() -> new ApiException(ReviewErrorCode.NO_REVIEW));
     Member member = memberRepository.getReferenceById(memberId);
 
     if (reviewMemoRepository.existsByReviewAndMember(review, member)) {
-      throw new ReviewException(ReviewErrorCode.MEMO_ALREADY_EXISTS);
+      throw new ApiException(ReviewErrorCode.MEMO_ALREADY_EXISTS);
     }
 
     ReviewMemo reviewMemo = ReviewMemoConverter.toReviewMemo(review, member, request);
