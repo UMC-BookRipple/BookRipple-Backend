2ffc8955b727caa31cf465753022c48e10a89dbc
오영훈 <skek2412@naver.com>
2026-01-21 20:47:49 +0900

    feat: ValidationGroups 추가  #24

---
+++ src/main/java/com/bookripple/api/global/validation/ValidationGroups.java
@@ -0,0 +1,12 @@
+package com.bookripple.api.global.validation;
+
+import jakarta.validation.groups.Default;
+
+public class ValidationGroups {
+
+  public interface QuestionGroup extends Default {
+
+  }
+
+
+}
