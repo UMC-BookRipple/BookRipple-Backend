3e7f205f63df90602cb767d4ebed076fb15db7e8
Inyoung <piypiy9102bbb@gmail.com>
2026-01-21 17:28:22 +0900

    add: Member 리포지토리 - 이메일 중복 체크를 위해 existsByEmail 추가

--- src/main/java/com/bookripple/api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/bookripple/api/domain/member/repository/MemberRepository.java
@@ -17,6 +17,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   // 아이디 중복 체크
   boolean existsByLoginId(String loginId);
 
+  // 이메일 중복 체크
+  boolean existsByEmail(String email);
+
   // 이메일 조회 (중복 체크 / OAuth 대비)
   Optional<Member> findByEmail(String email);
 
