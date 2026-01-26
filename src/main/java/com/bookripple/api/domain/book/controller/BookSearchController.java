c4172308f7f4b10fd643175b5a2c9bac9955738c
hyun-wjd <helencho0530@gmail.com>
2026-01-26 15:00:59 +0900

    fix: 컴파일 오류 해결

--- src/main/java/com/bookripple/api/domain/book/controller/BookSearchController.java
+++ src/main/java/com/bookripple/api/domain/book/controller/BookSearchController.java
@@ -7,6 +7,8 @@ import com.bookripple.api.common.response.ApiResponse;
 import com.bookripple.api.domain.book.dto.BookRes;
 import com.bookripple.api.domain.book.dto.BookSearchRes;
 import com.bookripple.api.domain.book.service.BookQueryService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,9 +22,9 @@ public class BookSearchController {
     private final BookQueryService bookQueryService;
 
     // 1) 알라딘 검색 API
-    @GetMapping("search")
+    @GetMapping("/search")
     public ApiResponse<BookSearchRes> search(
-            @RequestParam String keyword,
+            @RequestParam @NotBlank @Size(max = 100) String keyword,
             @RequestParam(defaultValue = "1") int start,
             @RequestParam(defaultValue = "20") int size,
             @RequestParam(defaultValue = "Keyword") String queryType,
