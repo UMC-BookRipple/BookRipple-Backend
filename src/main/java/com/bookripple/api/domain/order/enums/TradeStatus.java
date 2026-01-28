43c7855442d46788a4168dbdeecb3eb5d3e2c76f
Seojun Choi <tjwns7759@cau.ac.kr>
2026-01-29 03:45:17 +0900

    refactor: 주석 내용 변경

--- src/main/java/com/bookripple/api/domain/order/enums/TradeStatus.java
+++ src/main/java/com/bookripple/api/domain/order/enums/TradeStatus.java
@@ -3,7 +3,7 @@ package com.bookripple.api.domain.order.enums;
 public enum TradeStatus {
     REQUESTED, // 구매요청(결제 전)
     APPROVED, // 판매자 승인(결제 가능)
-    REJECTED, // 판매자
+    REJECTED, // 구매자가 거래를 취소할 때
     PAID,
     SHIPPED,
     COMPLETED
