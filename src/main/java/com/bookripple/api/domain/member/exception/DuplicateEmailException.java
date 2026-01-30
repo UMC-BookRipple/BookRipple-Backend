46a3ef3a3f68129657b38150910fdfa13c438412
Inyoung <piypiy9102bbb@gmail.com>
2026-01-30 19:03:52 +0900

    chore: 주석 추가

--- src/main/java/com/bookripple/api/domain/member/exception/DuplicateEmailException.java
+++ src/main/java/com/bookripple/api/domain/member/exception/DuplicateEmailException.java
@@ -2,7 +2,12 @@ package com.bookripple.api.domain.member.exception;
 
 import com.bookripple.api.common.code.CommonErrorCode;
 import com.bookripple.api.common.error.ApiException;
-
+/**
+ * 이메일 중복 시 발생
+ * - 회원가입
+ * - 이메일 변경
+ * - 소셜 로그인 연동 시
+ */
 public class DuplicateEmailException extends ApiException {
 
   public DuplicateEmailException() {
