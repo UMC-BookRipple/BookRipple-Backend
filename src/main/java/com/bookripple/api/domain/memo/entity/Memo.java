c72b5dd07776c70d7b10990ba7e70e1a3c9118b2
hyun-wjd <helencho0530@gmail.com>
2026-01-30 18:20:40 +0900

    ㄹfeat: 독서메모 ëã 엔티티 정의

---
+++ src/main/java/com/bookripple/api/domain/memo/entity/Memo.java
@@ -0,0 +1,34 @@
+package com.bookripple.api.domain.memo.entity;
+
+import com.bookripple.api.domain.book.entity.Book;
+import com.bookripple.api.domain.member.entity.Member;
+import com.bookripple.api.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "memo")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Builder
+public class Memo extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String context;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String page;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+}
