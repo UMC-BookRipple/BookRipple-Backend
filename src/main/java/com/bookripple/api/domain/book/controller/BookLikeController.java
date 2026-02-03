8239791442e6d2ae265326557c85492cebdaddf2
hyun-wjd <helencho0530@gmail.com>
2026-02-04 05:47:23 +0900

    ãㄹãfeat: 스웨거 문서 Definition 설정

--- src/main/java/com/bookripple/api/domain/book/controller/BookLikeController.java
+++ src/main/java/com/bookripple/api/domain/book/controller/BookLikeController.java
@@ -7,7 +7,12 @@ import com.bookripple.api.domain.book.service.BookCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(
+        name = "도서 좋아요",
+        description = "도서 좋아요 추가/삭제 API"
+)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/books/likes")
