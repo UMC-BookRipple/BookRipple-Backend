f5956481bdd546fefde510d7fa6129072ef760ac
Seojun Choi <tjwns7759@cau.ac.kr>
2026-02-04 21:42:52 +0900

    feat: 구매자용 블라인드 북 판매 목록 조회 화면 위한 repository 작성

--- src/main/java/com/bookripple/api/domain/blindsalepost/repository/PurchaseRequestRepository.java
+++ src/main/java/com/bookripple/api/domain/blindsalepost/repository/PurchaseRequestRepository.java
@@ -3,6 +3,7 @@ package com.bookripple.api.domain.blindsalepost.repository;
 import com.bookripple.api.domain.blindsalepost.entity.BlindSalePost;
 import com.bookripple.api.domain.blindsalepost.entity.PurchaseRequest;
 import com.bookripple.api.domain.member.entity.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,15 +11,19 @@ import java.util.Optional;
 
 public interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest, Long> {
 
-    // 1. 특정 게시글의 전체 구매 요청 리스트 조회 (명단 확인용)
+    // 특정 게시글의 전체 구매 요청 리스트 조회 (명단 확인용)
     List<PurchaseRequest> findAllByBlindSalePostId(Long blindSalePostId);
 
-    // 2. 특정 게시글의 판매 요청 인원수 카운트
+    // 특정 게시글의 판매 요청 인원수 카운트
     long countByBlindSalePostId(Long blindSalePostId);
 
-    // 3. 중복 요청 방지 확인 (이미 요청한 유저인지 체크)
+    // 중복 요청 방지 확인 (이미 요청한 유저인지 체크)
     boolean existsByBlindSalePostAndMember(BlindSalePost blindSalePost, Member member);
 
-    // 4. 특정 유저가 보낸 요청 하나 조회
+    // 특정 유저가 보낸 요청 하나 조회
     Optional<PurchaseRequest> findByBlindSalePostIdAndMemberId(Long blindSalePostId, Long memberId);
+
+    // 내가 보낸 요청 목록 조회
+    List<PurchaseRequest> findAllByMemberIdOrderByIdDesc(Long memberId, Pageable pageable);
+    List<PurchaseRequest> findAllByMemberIdAndIdLessThanOrderByIdDesc(Long memberId, Long id, Pageable pageable);
 }
