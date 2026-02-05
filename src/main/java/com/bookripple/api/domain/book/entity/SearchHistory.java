ef6664759a240d086f34464b88c614f3706d5479
hyun-wjd <helencho0530@gmail.com>
2026-02-05 14:38:52 +0900

    feat: 검색기록 entity

---
+++ src/main/java/com/bookripple/api/domain/book/entity/SearchHistory.java
@@ -0,0 +1,30 @@
+package com.bookripple.api.domain.book.entity;
+
+import com.bookripple.api.domain.member.entity.Member;
+import com.bookripple.api.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SearchHistory extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false, length = 100)
+    private String keyword;
+
+}
+
