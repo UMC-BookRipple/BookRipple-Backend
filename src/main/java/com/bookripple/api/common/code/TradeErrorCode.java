b62cc943dac8b6772195bd6370658e7951a9bd71
Seojun Choi <tjwns7759@cau.ac.kr>
2026-02-07 15:57:11 +0900

    feat: TradeErrorCode 추가

---
+++ src/main/java/com/bookripple/api/common/code/TradeErrorCode.java
@@ -0,0 +1,24 @@
+package com.bookripple.api.common.code;
+
+
+import org.springframework.http.HttpStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum TradeErrorCode implements BaseErrorCode {
+    // 404 NOT FOUND
+    TRADE_NOT_FOUND(404, "TRADE_404", "해당 거래를 찾을 수 없습니다."),
+    // 403 FORBIDDEN
+    NOT_TRADE_PARTICIPANT(403, "TRADE_403", "거래 참여자가 아닙니다."),
+    // 400 BAD REQUEST
+    INVALID_TRADE_STATUS(400, "TRADE_400_1", "올바르지 않은 거래 상태입니다."),
+    PAYMENT_ALREADY_PREPARED(400, "TRADE_400_2", "이미 결제 준비가 완료되었습니다."),
+    PAYMENT_NOT_COMPLETED(400, "TRADE_400_3", "결제가 완료되지 않았습니다."),
+    SETTLEMENT_ALREADY_COMPLETED(400, "TRADE_400_4", "이미 정산이 완료되었습니다.");
+
+    private final int httpStatus;
+    private final String code;
+    private final String message;
+}
