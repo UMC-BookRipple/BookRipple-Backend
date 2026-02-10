a9d70d90e6fa3d79314e9d9307a09777a9bb9fab
sseobi16 <jseobi3951@naver.com>
2026-02-10 22:59:11 +0900

    feat: 스케줄러를 사용하여 알림 구현 #57

--- src/main/java/com/bookripple/api/domain/reading/repository/ReadingProgressRepository.java
+++ src/main/java/com/bookripple/api/domain/reading/repository/ReadingProgressRepository.java
@@ -1,12 +1,11 @@
 package com.bookripple.api.domain.reading.repository;
 
 import com.bookripple.api.domain.reading.entity.ReadingProgress;
+import java.time.LocalDateTime;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ReadingProgressRepository extends JpaRepository<ReadingProgress, Long> {
@@ -16,4 +15,8 @@ public interface ReadingProgressRepository extends JpaRepository<ReadingProgress
   List<ReadingProgress> findByMemberIdAndIsLikedTrueAndIdLessThanOrderByIdDesc(Long memberId, Long lastId, Pageable pageable);
 
   List<ReadingProgress> findByMemberIdAndIsLikedTrueOrderByIdDesc(Long memberId, Pageable pageable);
+
+  List<ReadingProgress> findAllByIsCompletedFalseAndUpdatedAtBefore(LocalDateTime threshold);
+
+  List<ReadingProgress> findAllByIsCompletedTrueAndUpdatedAtBefore(LocalDateTime threshold);
 }
